009e2098b9a0f359cf0c9e426209247fa39858a0
mattrohr3 <mattrohr3@gmail.com>
2016-10-27 01:36:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /lifter$/;
-      botRegex = /beer$/;
+      botRegex = /lifter$/,/beer$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
