2ac2d9b4dc149fc0e1c0f91f037223f4c1c6457b
mattrohr3 <mattrohr3@gmail.com>
2016-10-30 20:56:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /where is|where are|when is|when are|how are we|where do|do we have|what time|when should|when does|what is|where should/;
+      botRegex = /er/;
   
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -19,7 +19,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "sigh" ;
+  botResponse = "I 'ardly know 'r" ;
 
   options = {
     hostname: 'api.groupme.com',
